05adfe64e807361c58144cdc1580734a45148c70
darkdevil3610 <94959564+darkdevil3610@users.noreply.github.com>
2022-07-01 11:47:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,23 +23,24 @@ global.language = english  //change indonesia to english if you don't understand
 
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.botname = "𝐺𝛩𝑈𝑅𝛥𝛻-Mdོ" //namabot kalian
+global.ownername= "ᴹᴿ᭄ 𝐺𝛩𝑈𝑅𝛥𝛻 ×፝֟͜×" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
+global.youtube = "https://instagram.com/dark_devil_3609" //bebas asal jan hapus
+global.github = "https://cutt.ly/mKSJt3s/" //bebas
 global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.timezone = 'Asia/Jakarta' //  timezone wib
-global.owner = ["622150996855","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.region = "Iindia" //bebas
+global.timezone = 'Asia/Kolkata' //  timezone wib
+global.owner = ["918089152280","917025770987"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "918089152280" // nomor wa kalian
+global.ownernomerr = "+918089152280" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© 𝐺𝛩𝑈𝑅𝛥𝛻' //sticker wm ubah
+global.author = 'dark_devil_3609'
+//sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
